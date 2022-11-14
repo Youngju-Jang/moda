07b3a66b385021f7996dbf39983e468224206e15
Youngju Jang <joj1043@nate.com>
2022-11-14 17:02:02 +0900

    base2

--- app.py
+++ app.py
@@ -5,5 +5,30 @@ import requests
 from bs4 import BeautifulSoup
 
 from pymongo import MongoClient
-# client = MongoClient('mongodb+srv://test:sparta@cluster0.qaukrbc.mongodb.net/?retryWrites=true&w=majority')
-# db = client.dbsparta
+client = MongoClient('mongodb+srv://test:sparta@cluster0.qaukrbc.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+
+########### 로그인 ##########
+@app.route('/login')
+def home():
+    return render_template('login.html')
+
+
+########### 로그인 ##########
+########### home ##########
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+########### home ##########
+########### 마이페이지 ##########
+@app.route("/mypage", methods=["GET"])
+def movie_get():
+    movie_list = list(db.movies.find({},{'_id':False}))
+    # return jsonify({'movies': movie_list})
+    return render_template('mypage.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
