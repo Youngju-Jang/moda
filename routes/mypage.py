6317a73a8ad9c55b42abde6fcad98f40eeb97a23
Youngju Jang <joj1043@nate.com>
2022-11-15 11:29:45 +0900

    - 임시 user 생성form 추가 - url 주소로 이미지바꾸기 추가 - 처음 주소접근시 "장영주" 로 user설정 (로그인 기능추가이후 세션에서 받아오게끔 변경필요)

--- routes/mypage.py
+++ routes/mypage.py
@@ -1,5 +1,10 @@
+import json
+
 from flask import Blueprint, Flask, render_template, request, jsonify
 from . import routes
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.qaukrbc.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
 
 @routes.route('/users')
 def users():
@@ -7,11 +12,27 @@ def users():
 
 @routes.route("/mypage", methods=["GET"])
 def mypage():
+    user = db.user.find_one({'id': '장영주'})
+    print("/mypage GET >>>>>>>>>>" , user)
+    return render_template('mypage.html', user=user)
+
+@routes.route("/mypage", methods=["POST"])
+def temp_usermake():
+    url_receive = request.form['url_give']
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+    doc = {'url': url_receive,
+           'id': id_receive,
+           'pw': pw_receive}
+
+    db.user.insert_one(doc)
     return render_template('mypage.html')
 
-@routes.route("/mypage", methods=['PATCH'])
+@routes.route("/mypage", methods=["PATCH"])
 def change_image():
-    print(">>>>>>>>>>>>>>", request.form)
-    form_receive = request.form['form_give']
-    print(form_receive)
-    return render_template('mypage.html')
+    url_receive = request.form['url_give']
+
+    # 로그인기능 생성 후 id 수정필요
+    db.user.update_one({'id': "장영주"}, {'$set': {'url': url_receive}})
+
+    return jsonify({'msg':".."});
