e541fa9d6528b959232ba9e5b577e664153fe5d3
MJ-Dev92 <minjae920802@naver.com>
2022-11-17 00:41:25 +0900

    게시물 분할 성공

--- routes/comment.py
+++ routes/comment.py
@@ -17,35 +17,17 @@ def comment():
 ## 코멘트 입력값 가져오기
 @routes.route("/comment/comment1", methods=["POST"])
 def comment_post():
+
     comment_receive = request.form['comment_give']
     user_receive = request.form['user_give']
 
-    write_num = db.comment.find_one({'num': 'bobby'})
-    max_num = db.comment_test.find_one({}, sort=[('comment_num', -1)])['comment_num']
+    write_num = db.comment.find_one({'write_num': int(7)})['write_num']
 
-    # max_num = 0
-    #
-    # if type(num) is not int:
-    #     pass
-    # else:
-    #     max_num = num['comment_num']
-
-    # count = max_num + 1
-    if len(max_num) == 0:
-        max_num = 0
-    count = max_num + 1
-
-    # max_num = 0
-    # temp = db.comment_test.find_one({}, sort=[('comment_num', -1)])
-    # if type(temp) is not int:
-    #     pass
-    # else:
-    #     max_num = db.comment_test.find_one({}, sort=[('comment_num', -1)])['comment_num']
-    #
-    # count = int(max_num) + 1
+    comment_test_list = list(db.comment_test.find({'write_num':int(7)}, {'_id': False}))
+    count = len(comment_test_list) + 1
 
     doc = {
-        # 'write_num' : num,
+        'write_num' : write_num,
         'comment_num': count,
         'user' : user_receive,
         'comment': comment_receive,
@@ -67,18 +49,21 @@ def comment_done():
 ## 댓글 정보 요청하기
 @routes.route("/comment/comment1", methods=["GET"])
 def commment_get():
+    num_receive = request.args.get('num_give')
 
-    comment_list = list(db.comment_test.find({},{'_id':False}))
+    comment_list = list(db.comment_test.find({'write_num': int(7)},{'_id':False}))
 
 
     return jsonify({'comment': comment_list})
 
 
-## 게시물 정보 요청하기
+
+# 게시물 정보 요청하기
 @routes.route("/comment/comment2", methods=["GET"])
 def text_get():
+    num_receive = request.args.get('num_give')
 
-    text_list = list(db.comment.find({}, {'_id': False}))
+    text_list = list(db.comment.find({'write_num': int(7)}, {'_id': False}))
 
     return jsonify({'text': text_list})
 
