30040fb03f1d7812f281efc1661d219c1fd3f65e
Youngju Jang <joj1043@nate.com>
2022-11-16 17:01:15 +0900

    코멘트아이콘 작업중

--- routes/mypage.py
+++ routes/mypage.py
@@ -29,24 +29,30 @@ def temp_usermake():
     db.user.insert_one(doc)
     return render_template('mypage.html')
 
+
 ###  임시_코멘트생성
 @routes.route("/temp_comment", methods=["POST"])
 def temp_commentMake():
     comment_receive = request.form['comment_give']
-    max_num = db.comment.find_one({}, sort=[('comment_num', -1)])['comment_num']
-    print(">>>>>>>>>temp_commentMake>>>>>>>>", max_num)
-    if type(max_num) != int :
-        max_num = 0
+    temp = db.comment.find_one({}, sort=[('comment_num', -1)])
+    max_num = 0
+
+    if type(temp) is not int:
+        pass
+    else:
+        max_num = temp['comment_num']
+
     count = max_num + 1
 
     doc = {'write_num': 7,
            'comment_num': count,
-           'comment' : comment_receive,
-           'user' : '장영주'
+           'comment': comment_receive,
+           'user': '장영주'
            }
 
     db.comment.insert_one(doc)
     return jsonify({'msg': ".."});
+
 
 ###  이미지변경
 @routes.route("/mypage", methods=["PATCH"])
@@ -63,7 +69,12 @@ def change_image():
 @routes.route("/temp_makeDiray", methods=["POST"])
 def temp_makeDiray():
     diary_receive = request.form['diary_give']
-    max_num = db.write.find_one({}, sort=[('write_num', -1)])['write_num']
+    max_num = 0
+    temp = db.write.find_one({}, sort=[('write_num', -1)])
+    if type(temp) is not int:
+        pass
+    else:
+        max_num = db.write.find_one({}, sort=[('write_num', -1)])['write_num']
 
     count = max_num + 1
 
@@ -77,7 +88,6 @@ def temp_makeDiray():
     return jsonify({'msg': '등록 완료!'})
 
 
-
 ###  글가져오기
 @routes.route("/temp_diary", methods=["GET"])
 def diary_get():
@@ -87,20 +97,20 @@ def diary_get():
     pipeline = [
         {
             "$lookup": {
-            "from" : "comment",
-            "localField" : "write_num",
-            "foreignField" : "write_num",
-            "as" : "commentInfo"
+                "from": "comment",
+                "localField": "write_num",
+                "foreignField": "write_num",
+                "as": "commentInfo"
             },
         },
         {
-            "$project":{
-                "_id":0,
-                "user":1,
-                "text":1,
-                "good":1,
-                "write_num":1,
-                "commentInfo":1,
+            "$project": {
+                "_id": 0,
+                "user": 1,
+                "text": 1,
+                "good": 1,
+                "write_num": 1,
+                "commentInfo": 1,
             }
         }
     ]
@@ -127,11 +137,10 @@ def good_update():
     good_receive = request.form['user_give']  # "장영주"
     write_num = request.form['num_give']
     good_give = request.form['good_give']  # t or f
-    if good_give == 'true' :
+    if good_give == 'true':
         db.write.update_one({'write_num': int(write_num)}, {"$push": {'good': good_receive}})
-    else :
-        db.write.update_one({'write_num':int(write_num)},{"$pull": {'good': good_receive}})
-
+    else:
+        db.write.update_one({'write_num': int(write_num)}, {"$pull": {'good': good_receive}})
 
     # 로그인기능 생성 후 id 수정필요
     # db.user.update_one({'id': "장영주"}, {'$set': {'url': }})
