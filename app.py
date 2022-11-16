fb2345beb6c54eb9fb6b4988206d450f323c7fbd
YOOsangryun <115338631+YOOsangryun@users.noreply.github.com>
2022-11-16 19:55:27 +0900

    sang

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from flask import Flask, render_template, request, jsonify
-from routes import *
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for
+
 app = Flask(__name__)
-app.register_blueprint(routes)
+
+from pymongo import MongoClient
+
 import certifi
+
 ca=certifi.where()
+
+client = MongoClient("mongodb+srv://dbtkdfbs:dbthvkd01!@cluster0.ylde6ne.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=ca)
+db = client.dbsparta_plus_week4
+
+SECRET_KEY = 'SPARTA'
+
 import jwt
 import datetime
 import hashlib
-SECRET_KEY = 'SPARTA'
-
-from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:sparta@cluster0.qaukrbc.mongodb.net/?retryWrites=true&w=majority')
-db = client.dbsparta
 
 @app.route('/')
 def home():
@@ -31,38 +35,30 @@ def login():
     msg = request.args.get("msg")
     return render_template('login.html', msg=msg)
 
-
 @app.route('/register')
 def register():
     return render_template('register.html')
 
 @app.route('/api/register', methods=['POST'])
 def api_register():
-
-
     id_receive = request.form['id_give']
     pw_receive = request.form['pw_give']
     nickname_receive = request.form['nickname_give']
-
     pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
 
-    db.user.insert_one({'id': id_receive, 'pw': pw_hash, 'nick': nickname_receive})
-
-    return jsonify({'result': 'success'})
-
-
+    result = db.user.find_one({'id': id_receive})
+    if result is not None:
+        return jsonify({'result': 'fail', 'msg': '이미 존재하는 ID입니다!'})
+    else:
+        db.user.insert_one({'id': id_receive, 'pw': pw_hash, 'nick': nickname_receive})
+        return jsonify({'result': 'success'})
 
 @app.route('/api/login', methods=['POST'])
 def api_login():
     id_receive = request.form['id_give']
     pw_receive = request.form['pw_give']
-
-
     pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
-
-
     result = db.user.find_one({'id': id_receive, 'pw': pw_hash})
-
 
     if result is not None:
 
@@ -72,34 +68,25 @@ def api_login():
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
 
-
         return jsonify({'result': 'success', 'token': token})
-
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
-
 
 @app.route('/api/nick', methods=['GET'])
 def api_valid():
     token_receive = request.cookies.get('mytoken')
 
-
-
     try:
+
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
         print(payload)
 
         userinfo = db.user.find_one({'id': payload['id']}, {'_id': 0})
         return jsonify({'result': 'success', 'nickname': userinfo['nick']})
     except jwt.ExpiredSignatureError:
-        # 위를 실행했는데 만료시간이 지났으면 에러가 납니다.
         return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
     except jwt.exceptions.DecodeError:
         return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
-
-
-
-
 
 
 
