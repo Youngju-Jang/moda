28543ff32c24efb3f4f511735d74af82750e3d48
hannahcodes0 <hannahhayoungkim2@gmail.com>
2022-11-17 11:47:25 +0900

    css 수정-

--- routes/home.py
+++ routes/home.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
 from . import routes
 
 import certifi
@@ -8,8 +9,6 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:sparta@cluster0.bckltib.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
 
-from flask import Flask, render_template, request, jsonify
-app = Flask(__name__)
 
 ########### home ###########
 @routes.route('/home')
@@ -23,13 +22,13 @@ def web_mars_post():
     doc={
         'name': name_receive,
     }
-    db.home.insert_one(doc)
+    db.write.insert_one(doc)
 
     return jsonify({'msg': '작성 완료!'})
 
 @routes.route("/home", methods=["GET"])
 def web_mars_get():
-    order_list = list(db.home.find({}, {'_id': False}))
+    order_list = list(db.write.find({}, {'_id': False}))
     return jsonify({'orders': order_list})
 
 if __name__ == '__main__':
